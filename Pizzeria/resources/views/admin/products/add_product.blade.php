87464f1598b48a129e5432748ac0918697b02026
Demonq2004 <grzegorzekdawid@gmail.com>
2021-09-24 11:22:53 +0200

    product-form and product-list

--- Pizzeria/resources/views/admin/products/add_product.blade.php
+++ Pizzeria/resources/views/admin/products/add_product.blade.php
@@ -6,6 +6,26 @@
             <h1 class="heading">Blog Home Page Heading</h1>
         </header>
         <article class="content px-3 py-5 p-md-5">
-            <p>Fromularz dodawania produktow</p>
+            <h1>Fromularz dodawania produktow</h1>
+            <form method="post">
+                <div class="form-group">
+                    <label for="exampleInputProduct">Nazwa Produktu</label>
+                    <input type="text" class="form-control" name="product" id="exampleInputProduct" placeholder="Produkt" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputProductPrice">Cena Produktu</label>
+                    <input type="number" class="form-control" name="price" id="exampleInputProductsPrice" placeholder="Cena" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputProductSupplier">Dostawca Produktu</label>
+                    <input type="text" class="form-control" name="supplier" id="exampleInputProductSupplier" placeholder="Dostawca" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputProductExpirationDate">Data Ważności Produktu</label>
+                    <input type="date" class="form-control" name="supplier" id="exampleInputProductExpirationDate" placeholder="Data Ważności"  aria-describedby="dateHelp" required>
+                    <small id="dateHelp" class="from-text text-muted">Przykładnowa data: 2022-05-01</small>
+                </div>
+                <button type="submit" class="btn btn-primary">Dodaj Produkt</button>
+            </form>
         </article>
 @endsection()
--- Pizzeria/resources/views/admin/products/list.blade.php
+++ Pizzeria/resources/views/admin/products/list.blade.php
@@ -6,6 +6,30 @@
             <h1 class="heading">Blog Home Page Heading</h1>
         </header>
         <article class="content px-3 py-5 p-md-5">
-            <p>Lista produktów</p>
+            <h1>Lista produktów</h1>
+            <table class="table">
+                <thead class="table-light">
+                    <tr>
+                        <th scope="col">Nazwa</th>
+                        <th scope="col">Cena</th>
+                        <th scope="col">Dostawca</th>
+                        <th scope="col">Data Ważności</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Pomidor</td>
+                        <td>2zł</td>
+                        <td>Hurtownia Katowice</td>
+                        <td>08-10-2021</td>
+                    </tr>
+                    <tr>
+                        <td>Ser</td>
+                        <td>5zł</td>
+                        <td>Serownia "Pod Grzybem"</td>
+                        <td>12-11-2021</td>
+                    </tr>
+                </tbody>
+            </table>
         </article>
 @endsection()
